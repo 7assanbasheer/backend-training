483e4ea27c51f9c152e36491d99b3b67ee56d053
hassan basheer alshakhouri <7assan.basheer@gmail.com>
2024-06-30 02:49:23 +0300

    middleware

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log(10);
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/", (req, res) => {
+  res.send({
+    msg: "ارحب",
+  });
+});
+
+app.get("/hello", (req, res) => {
+  res.send({
+    msg: "h",
+  });
+});
+
+app.get("/person/:name", (req, res) => {
+  const name = req.params.name;
+  res.send({
+    name,
+  });
+});
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log({
+    username,
+    email,
+    password,
+  });
+
+  res.send();
+});
+
+app.listen(3000);
